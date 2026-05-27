47364618c9e3c6cf181d0ef29a6e6d87174befe8
dpendleton1 <daniel.pendleton@gmail.com>
2026-05-27 16:10:18 -0400

    delete junk files

--- dan_tidbits_notes.py
+++
@@ -1,4 +0,0 @@
-# SiteCode prefixes to keep
-
-# Fort Point, Wilson Point, Dyce Head, Lincolnville, Eggamoggin Reach, Mullen's Head, Owl's Head
-'FP','FTPN','WPLP','WP-','WP0','DH','LH','ER','MH','OH'
