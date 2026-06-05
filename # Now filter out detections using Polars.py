f7f03a670266089044d5b96515f36c05f5cfe455
dpendleton1 <daniel.pendleton@gmail.com>
2026-06-05 17:38:38 -0400

    delete confusing files

--- # Now filter out detections using Polars.py
+++
@@ -1,8 +0,0 @@
-# Now filter out detections using Polars
-import polars as pl
-
-df = pl.df('Smolts 2013-15.csv')
-#df_filtered = df.filter(pl.col('Event') != 'Detection')
-df_filtered = df.filter(pl.col('Event') != 'Detection')
-
-df.head()
--- OTN-analysis.py
+++
@@ -1,5 +0,0 @@
-#OTN analysis
-import pandas as pd
-
-df_otn = pd.read_csv('data/OTN_2008-2015.csv')
-print(df_otn.head())
