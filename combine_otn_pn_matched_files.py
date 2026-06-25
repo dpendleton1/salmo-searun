eb027f604d137febee05e1a97f3cc545261c8b8c
dpendleton1 <daniel.pendleton@gmail.com>
2026-06-25 12:27:03 -0400

    now creates dat_otn without duplicates or SourceFile column

--- combine_otn_pn_matched_files.py
+++ combine_otn_pn_matched_files.py
@@ -18,24 +18,21 @@ for f in files:
 
 dat_otn = pd.concat(data_frames, ignore_index=True)
 
-# Create a list of all columns except 'SourceFile'
-subset_cols = [c for c in dat_otn.columns if c != 'SourceFile']
-
-# Find duplicates based only on those columns
-duplicates = dat_otn[dat_otn.duplicated(subset=subset_cols, keep=False)]
-duplicates.to_csv(data_dir / "dat_otn_duplicates.csv", index=False)
-
-
-
 # there should not be any duplicate records accross files, but there are. we must correct this:
 # check for duplicate rows in concatenated dataframe
 # Show all rows that are duplicates of another row
-duplicates = dat_otn[dat_otn.duplicated(keep=False)]
-# See how many total duplicate rows exist
-total_duplicates = dat_otn.duplicated().sum()
-print(total_duplicates)
+# We do not want to include SourceFile in the subset for finding duplicates, as we want to find duplicates across files
+# Create a list of all columns except 'SourceFile'
+subset_cols = [c for c in dat_otn.columns if c != 'SourceFile']
+# Find duplicates based only the subset of columns not including SourceFile
+duplicates = dat_otn[dat_otn.duplicated(subset=subset_cols, keep=False)]
+# write out a .csv file showing all duplicates, and include 'SourceFile' so we can see which files the duplicates came from
+duplicates.to_csv(data_dir / "dat_otn_duplicates.csv", index=False)
+
 # Remove all rows that are exact duplicates
 dat_otn = dat_otn.drop_duplicates()
+# We no longer need the SourceFile column, so we can drop it, along with all the duplicate rows. 
+dat_otn = dat_otn.drop(columns=['SourceFile'])
 
 # Cast depth columns to float; invalid/empty strings become NaN
 dat_otn["bottomDepth"] = pd.to_numeric(dat_otn["bottomDepth"], errors="coerce")
@@ -50,15 +47,6 @@ dat_otn["dateCollectedUTC"] = dat_otn["dateCollectedUTC"].dt.tz_localize(None)
 # Extract the digits after the last dash in tagName as an integer ID code
 # e.g. A69-1601-8651 -> 8651
 dat_otn["IDCode"] = dat_otn["tagName"].str.split("-").str[-1].astype("Int64")
-
-# # Drop columns not needed for this analysis
-# dat_otn.drop(columns=[
-#     "collectionCode", "dateLastModified", "geometry","sensorRaw",
-#     "sensorName", "sensorType", "sensorValue", "sensorUnit",
-#     "geodeticDatum", "uncorrectedDateCollectedUTC", "contactPOC", "contactPI",
-#     "citation","codeSpace","scientificName","commonName","detectedBy",
-#     'receiverDepth','bottomDepth', 'localArea'
-# ], inplace=True)
 
 # add Year column based on the year of the first detection time (FirstTS)
 dat_otn['Year'] = dat_otn['dateCollectedUTC'].dt.year
